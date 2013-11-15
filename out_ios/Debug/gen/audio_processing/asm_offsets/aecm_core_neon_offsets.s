49b986aa750a517370c152d4d7e1722ccac73dc6
Bob Warwick <warwick@Serguei.local>
2013-11-15 13:54:14 -0800

    Now that we're on a better network connection, here's the full contents of our libjingle working directory.

---
+++ out_ios/Debug/gen/audio_processing/asm_offsets/aecm_core_neon_offsets.s
@@ -0,0 +1,54 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.section	__TEXT,__textcoal_nt,coalesced,pure_instructions
+	.section	__TEXT,__const_coal,coalesced
+	.section	__TEXT,__picsymbolstub4,symbol_stubs,none,16
+	.section	__TEXT,__StaticInit,regular,pure_instructions
+	.syntax unified
+	.section	__DATA,__data
+	.globl	_offset_aecm_dfaCleanQDomain @ @offset_aecm_dfaCleanQDomain
+	.align	2
+_offset_aecm_dfaCleanQDomain:
+	.long	13980                   @ 0x369c
+
+	.globl	_offset_aecm_outBuf     @ @offset_aecm_outBuf
+	.align	2
+_offset_aecm_outBuf:
+	.long	15992                   @ 0x3e78
+
+	.globl	_offset_aecm_xBuf       @ @offset_aecm_xBuf
+	.align	2
+_offset_aecm_xBuf:
+	.long	15980                   @ 0x3e6c
+
+	.globl	_offset_aecm_dBufNoisy  @ @offset_aecm_dBufNoisy
+	.align	2
+_offset_aecm_dBufNoisy:
+	.long	15988                   @ 0x3e74
+
+	.globl	_offset_aecm_dBufClean  @ @offset_aecm_dBufClean
+	.align	2
+_offset_aecm_dBufClean:
+	.long	15984                   @ 0x3e70
+
+	.globl	_offset_aecm_channelStored @ @offset_aecm_channelStored
+	.align	2
+_offset_aecm_channelStored:
+	.long	15968                   @ 0x3e60
+
+	.globl	_offset_aecm_channelAdapt16 @ @offset_aecm_channelAdapt16
+	.align	2
+_offset_aecm_channelAdapt16:
+	.long	15972                   @ 0x3e64
+
+	.globl	_offset_aecm_channelAdapt32 @ @offset_aecm_channelAdapt32
+	.align	2
+_offset_aecm_channelAdapt32:
+	.long	15976                   @ 0x3e68
+
+	.globl	_offset_aecm_real_fft   @ @offset_aecm_real_fft
+	.align	2
+_offset_aecm_real_fft:
+	.long	17220                   @ 0x4344
+
+
+.subsections_via_symbols
---
+++ out_ios/Debug/gen/audio_processing/asm_offsets/nsx_core_neon_offsets.s
@@ -0,0 +1,99 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.section	__TEXT,__textcoal_nt,coalesced,pure_instructions
+	.section	__TEXT,__const_coal,coalesced
+	.section	__TEXT,__picsymbolstub4,symbol_stubs,none,16
+	.section	__TEXT,__StaticInit,regular,pure_instructions
+	.syntax unified
+	.section	__DATA,__data
+	.globl	_offset_nsx_anaLen      @ @offset_nsx_anaLen
+	.align	2
+_offset_nsx_anaLen:
+	.long	3112                    @ 0xc28
+
+	.globl	_offset_nsx_anaLen2     @ @offset_nsx_anaLen2
+	.align	2
+_offset_nsx_anaLen2:
+	.long	3116                    @ 0xc2c
+
+	.globl	_offset_nsx_normData    @ @offset_nsx_normData
+	.align	2
+_offset_nsx_normData:
+	.long	13120                   @ 0x3340
+
+	.globl	_offset_nsx_analysisBuffer @ @offset_nsx_analysisBuffer
+	.align	2
+_offset_nsx_analysisBuffer:
+	.long	8                       @ 0x8
+
+	.globl	_offset_nsx_synthesisBuffer @ @offset_nsx_synthesisBuffer
+	.align	2
+_offset_nsx_synthesisBuffer:
+	.long	520                     @ 0x208
+
+	.globl	_offset_nsx_blockLen10ms @ @offset_nsx_blockLen10ms
+	.align	2
+_offset_nsx_blockLen10ms:
+	.long	12084                   @ 0x2f34
+
+	.globl	_offset_nsx_window      @ @offset_nsx_window
+	.align	2
+_offset_nsx_window:
+	.long	4                       @ 0x4
+
+	.globl	_offset_nsx_real        @ @offset_nsx_real
+	.align	2
+_offset_nsx_real:
+	.long	12088                   @ 0x2f38
+
+	.globl	_offset_nsx_imag        @ @offset_nsx_imag
+	.align	2
+_offset_nsx_imag:
+	.long	12600                   @ 0x3138
+
+	.globl	_offset_nsx_noiseSupFilter @ @offset_nsx_noiseSupFilter
+	.align	2
+_offset_nsx_noiseSupFilter:
+	.long	1032                    @ 0x408
+
+	.globl	_offset_nsx_magnLen     @ @offset_nsx_magnLen
+	.align	2
+_offset_nsx_magnLen:
+	.long	3120                    @ 0xc30
+
+	.globl	_offset_nsx_noiseEstLogQuantile @ @offset_nsx_noiseEstLogQuantile
+	.align	2
+_offset_nsx_noiseEstLogQuantile:
+	.long	1300                    @ 0x514
+
+	.globl	_offset_nsx_noiseEstQuantile @ @offset_nsx_noiseEstQuantile
+	.align	2
+_offset_nsx_noiseEstQuantile:
+	.long	2854                    @ 0xb26
+
+	.globl	_offset_nsx_qNoise      @ @offset_nsx_qNoise
+	.align	2
+_offset_nsx_qNoise:
+	.long	12072                   @ 0x2f28
+
+	.globl	_offset_nsx_stages      @ @offset_nsx_stages
+	.align	2
+_offset_nsx_stages:
+	.long	3128                    @ 0xc38
+
+	.globl	_offset_nsx_blockIndex  @ @offset_nsx_blockIndex
+	.align	2
+_offset_nsx_blockIndex:
+	.long	5548                    @ 0x15ac
+
+	.globl	_offset_nsx_noiseEstCounter @ @offset_nsx_noiseEstCounter
+	.align	2
+_offset_nsx_noiseEstCounter:
+	.long	2848                    @ 0xb20
+
+	.globl	_offset_nsx_noiseEstDensity @ @offset_nsx_noiseEstDensity
+	.align	2
+_offset_nsx_noiseEstDensity:
+	.long	2074                    @ 0x81a
+
+
+.subsections_via_symbols
